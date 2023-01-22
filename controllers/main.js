994d0a359217f1e459686d9df8abf324a17bb883
amer200 <amermostaafa@gmail.com>
2023-01-22 19:33:54 +0200

    commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,6 @@
 exports.getDash = (req, res) => {
     res.render('main/dashbord/index')
 }
+exports.getSignUP = (req, res) => {
+    res.render('main/dashbord/signup')
+}
--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,6 @@ const routes = express.Router();
 const mainController = require('../controllers/main');
 
 routes.get('/', mainController.getDash);
-
+routes.get('/signup', mainController.getSignUP)
 
 module.exports = routes
